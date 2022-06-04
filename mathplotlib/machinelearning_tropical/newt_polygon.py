faac17f27806dbc5d1571396db5d8f1338e65cfc
Gabriele Balletti <gabriele.balletti@gleechi.com>
2022-06-05 00:26:24 +0200

    Add netwon polygon script

---
+++ mathplotlib/machinelearning_tropical/newt_polygon.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+ax = plt.subplot()
+
+ax.plot([0, 1, 2, 2, 1, 0, 0], [0, 0, 1, 2, 2, 1, 0], color='black')
+ax.plot(0, 0, 'o', color='black')
+ax.plot(1, 0, 'o', color='black')
+ax.plot(2, 1, 'o', color='black')
+ax.plot(2, 2, 'o', color='black')
+ax.plot(1, 2, 'o', color='black')
+ax.plot(0, 1, 'o', color='black')
+
+ax.annotate("$(0,0)$", (0, 0), xytext=(0, 0 - 0.2))
+ax.annotate("$(1,0)$", (1, 0), xytext=(1, 0 - 0.2))
+ax.annotate("$(2,1)$", (2, 1), xytext=(2, 1 - 0.2))
+ax.annotate("$(2,2)$", (2, 2), xytext=(2, 2 - 0.2))
+ax.annotate("$(1,2)$", (1, 2), xytext=(1, 2 - 0.2))
+ax.annotate("$(0,1)$", (0, 1), xytext=(0, 1 - 0.2))
+
+#ax.set_title('')
+
+ax.axis('equal')
+ax.axis('off')
+
+plt.show()
